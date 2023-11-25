d0d88c64cf320b9a7ed65d1caf7b7a92af256363
V1centR <vicentcdb@gmail.com>
2023-11-24 23:27:19 -0300

    Apply services interface + safe component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,13 @@ import { HeaderTopComponent } from './header-top/header-top.component';
 import { FooterBottomComponent } from './footer-bottom/footer-bottom.component';
 import { RegisterComponent } from './register/register.component';
 import {InputTextModule} from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
 import { NgxCurrencyDirective } from 'ngx-currency';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { registerLocaleData, CommonModule  } from '@angular/common';
 import ptBr from '@angular/common/locales/pt';
 import { SafeComponent } from './safe/safe.component';
+import { MessagesModule } from 'primeng/messages';
 
 registerLocaleData(ptBr);
 @NgModule({
@@ -41,7 +43,9 @@ registerLocaleData(ptBr);
     InputTextModule,
     NgxCurrencyDirective,
     RadioButtonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    InputNumberModule,
+    MessagesModule
     
   ],
   providers: [
--- src/app/safe/safe.component.ts
+++ src/app/safe/safe.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from 'primeng/api/message';
 
 @Component({
   selector: 'app-safe',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './safe.component.css'
 })
 export class SafeComponent {
+
+  valueSafe:any;
+  alertMessage:Message[] | undefined;
+
+  ngOnInit() {
+    this.alertMessage = [{ severity: 'info', summary: 'OBS:', detail: 'Valor sendo poupado.' }];
+}
 
 }
